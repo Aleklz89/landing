32d54b04f6850a041217ac16318adfadfa6e118b
Aleklz89 <108018670+Aleklz89@users.noreply.github.com>
2024-05-30 14:47:18 +0300

    fixes

--- app/components/second/Second.js
+++ app/components/second/Second.js
@@ -6,17 +6,16 @@ function Second() {
     return (
         <div className={styles.main}  id="section1">
             <div className={styles.left}>
-                <div className={styles.title}>
-                    <span className={styles.blueText}>Платежі на картку</span>
-                    <span className={styles.whiteText}> можуть<br /> бути корисні для:</span>
+                <div className={styles.title}>blueText
+                    <span className={styles.whiteText}>Чому вартно обрати</span>
+                    <span className={styles.blueText}> нас?</span>
                 </div>
                 <div className={styles.table}>
-                    <div className={styles.row}>1. Оплати Google/Facebook/In-app реклами</div>
-                    <div className={styles.row}>2. Оплати холдів</div>
-                    <div className={styles.row}>3. Швидкого повернення холдів</div>
-                    <div className={styles.row}>4. Кешбеку за оплату Google/Facebook</div>
-                    <div className={styles.row}>5. Самостійного випуску карток</div>
-                    <div className={styles.end}>6. Безлічі методів поповнення</div>
+                    <div className={styles.row}>1. Швидкий та приємний саппорт</div>
+                    <div className={styles.row}>2. Найнижчий відсоток поповнення</div>
+                    <div className={styles.row}>3. Досвідчена команда фінансистів</div>
+                    <div className={styles.row}>4. Прозорі умови співпраці</div>
+                    <div className={styles.end}>5. Жодних прихованих платежів та комісій</div>
                 </div>
             </div>
             <div className={styles.right}>
